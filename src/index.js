3e0f482a47e8eeb01cfde760ae4ffeb7829b0d38
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2019-12-22 21:15:08 -0600

    Add comment to mark the removal of App function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// SJ0221219 - Replace below App function with my own function
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
