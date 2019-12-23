c2e33ccac7ba9a490681d71d717761a3331521c1
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2019-12-23 14:49:39 -0600

    Remove unused comment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// SJ0221219 - Replace below App function with my own function
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
