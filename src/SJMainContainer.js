5f84d2b5a8c6afda9e954a2210ff5e34e4df6047
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2020-01-15 19:28:43 -0600

    Remove alignItems

--- src/SJMainContainer.js
+++ src/SJMainContainer.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import IntroductionMessage from './components/IntroductionMessage';
+import PersonalInfo from './components/PersonalInfo';
 
 const SJMainContainer = () => {
 
   return(
-    <Grid container direction="row" alignItems="baseline">
+    // <Grid container direction="row" alignItems="baseline">
+    <Grid container direction="row" >
       <Grid item={true} xs={3}>
         <IntroductionMessage />
       </Grid>
 
       <Grid item={true} xs={9}>
-        <p>Para</p>
+        <PersonalInfo />
       </Grid>
-  </Grid>
+    </Grid>
   );
 }
 
