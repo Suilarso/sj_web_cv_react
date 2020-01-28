da36eb7ff5d1c8aa9ae07ceea1238e7af868589b
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2020-01-27 18:56:14 -0600

    Remark unused import statement

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+// import {createStyles, Paper, Typography, makeStyles, WithStyles, Checkbox, IconButton} from '@material-ui/core';
 import emailIcon from "../res/img/email-icon.png";
 import githubIcon from "../res/img/github-icon.png";
 import linkedinIcon from "../res/img/linkedin-icon.png";
@@ -12,6 +13,7 @@ const useStyles = makeStyles(theme => ({
     //   margin: theme.spacing(5),
     // },
     marginTop: theme.spacing(5),
+    // paddingTop: theme.spacing(5),
     // size: {width: 125, height: 150}
   },
   infoContainer: {
@@ -25,7 +27,7 @@ const PersonalInfo = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <section className={classes.root}>
       <div className={classes.infoContainer}>
         <img src={linkedinIcon} alt="linkedin" title="linkedin"></img>
         <a href="https://www.linkedin.com/in/suilarso-japit-04b343140" target="_blank">LinkedIn URL</a>
@@ -45,7 +47,7 @@ const PersonalInfo = () => {
         <img src={phoneIcon} alt="Mobile" title="Mobile"></img>
         <span>(431) 336 4539</span>
       </div>
-    </div>
+    </section>
   );
 
 }
