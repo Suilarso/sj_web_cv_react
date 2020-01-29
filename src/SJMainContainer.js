cfd398553d13ec792ee9b9f0a545c7283bd5dd6a
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2020-01-29 14:58:10 -0600

    Add initial BriefSummary file to display summary info

--- src/SJMainContainer.js
+++ src/SJMainContainer.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import IntroductionMessage from './components/IntroductionMessage';
 import PersonalInfo from './components/PersonalInfo';
+import BriefSummary from './components/BriefSummary';
 
 const SJMainContainer = () => {
 
   return(
     // <Grid container direction="row" alignItems="baseline">
-    <Grid container direction="row" >
-      <Grid item={true} xs={3}>
-        <IntroductionMessage />
+    <Grid container>
+      <Grid container direction="row" >
+        <Grid item={true} xs={3}>
+          <IntroductionMessage />
+        </Grid>
+
+        <Grid item={true} xs={9}>
+          <PersonalInfo />
+        </Grid>
       </Grid>
 
-      <Grid item={true} xs={9}>
-        <PersonalInfo />
+      <Grid container>
+        <Grid item={true} xs={12}>
+          <BriefSummary />
+        </Grid>
       </Grid>
     </Grid>
   );
